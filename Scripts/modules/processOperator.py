0eecd2c5a6a6e9441a54468e599b9c524c73ed30
Krazerleo <krazerleo@gmail.com>
2023-04-16 22:13:13 +0700

    Revert "Revert "Revert "added automatic render output"""
    This reverts commit 1a0ed99e08444d49d7c4a3cd953fd68a98e5e52a.

--- Scripts/modules/processOperator.py
+++ Scripts/modules/processOperator.py
@@ -1,17 +1,7 @@
-from datetime import datetime
 import sys
-import os
-import pathlib
 import bpy
 
 from processing import SimExecutioner
-
-def Render(output_file_pattern_string = 'render_{time}.jpg'):
-    output_dir = pathlib.Path().resolve() / 'Samples'
-    now = datetime.now().strftime('%Y-%m-%d')
-    bpy.context.scene.render.filepath = os.path.join(output_dir, output_file_pattern_string.format(time=now))
-    bpy.ops.render.render(write_still = True)
-    return
 
 class BottleSimOperator(bpy.types.Operator):
     """Make Sample"""
@@ -19,14 +9,15 @@ class BottleSimOperator(bpy.types.Operator):
     bl_label  = "Create Trash Sample"
 
     def execute(self, context):
-        output_path = pathlib.Path().resolve() / 'Samples'
         se = SimExecutioner()
         se.Process()
-        Render()
         return {'FINISHED'}
 
     def invoke(self, context, event):
-        return self.execute(context)
+        print("chin chin")
+        se = SimExecutioner()
+        se.Process()
+        return {'RUNNING_MODAL'}
 
 
 def register():
@@ -36,5 +27,5 @@ def register():
 def unregister():
     bpy.utils.unregister_class(BottleSimOperator)
 
-if __name__ == "__main__":
-    register()
+
+bpy.utils.register_class(BottleSimOperator)
