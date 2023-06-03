a76798f04ca9b56426be80a1bd17b536a996195a
Krazerleo <krazerleo@gmail.com>
2023-06-03 14:45:21 +0700

    improved can material

--- run_simulation.py
+++ run_simulation.py
@@ -1,4 +1,6 @@
 import bpy
 
-bpy.ops.utils.execute_simulation(deform_frames = 41, falling_frames = 100, 
-    water_frames = 100, bottle_type = 'ANY', water_resolution = 7, deform_force = 0.4)
+for i in range(0, 10):
+    bpy.ops.utils.execute_simulation(deform_frames = 41, falling_frames = 100, 
+                                    water_frames = 100, bottle_type = 'ANY', 
+                                    water_resolution = 7, deform_force = 0.4)
